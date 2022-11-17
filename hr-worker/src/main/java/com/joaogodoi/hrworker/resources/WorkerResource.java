b7619a5cf6be4a595f163f24da122cfd2a5b2dbd
João Godoi <joaogodoi.dev@gmail.com>
2022-11-17 14:48:58 -0300

    :sparkles: Feat: Configured actuator for runtime config refresh

--- hr-worker/src/main/java/com/joaogodoi/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/joaogodoi/hrworker/resources/WorkerResource.java
@@ -2,7 +2,11 @@ package com.joaogodoi.hrworker.resources;
 
 import com.joaogodoi.hrworker.entities.Worker;
 import com.joaogodoi.hrworker.repositories.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,11 +14,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Optional;
 
+@RefreshScope
 @RestController
 @RequestMapping(value = "/worker")
 public class WorkerResource {
+
+    private static final Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+
+    @Value("${test.config}")
+    private String testConfig;
 
     @Autowired
     private WorkerRepository workerRepository;
@@ -29,5 +38,11 @@ public class WorkerResource {
     public ResponseEntity<Worker> getWorker(@PathVariable Long id) {
         Worker worker = workerRepository.findById(id).get();
         return ResponseEntity.ok(worker);
+    }
+
+    @GetMapping(value = "/config")
+    public ResponseEntity<Void> getConfig() {
+        logger.info("CONFIG = " + testConfig);
+        return ResponseEntity.noContent().build();
     }
 }
