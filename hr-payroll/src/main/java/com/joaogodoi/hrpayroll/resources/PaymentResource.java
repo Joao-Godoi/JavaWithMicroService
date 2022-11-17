dacce7941a9535d2b7ce14dbd02b749208cb76e8
João Godoi <joaogodoi.dev@gmail.com>
2022-11-17 02:44:41 -0300

    :sparkles: Feat: Configurated a circuit breaker on payroll service

--- hr-payroll/src/main/java/com/joaogodoi/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/joaogodoi/hrpayroll/resources/PaymentResource.java
@@ -2,6 +2,7 @@ package com.joaogodoi.hrpayroll.resources;
 
 import com.joaogodoi.hrpayroll.entities.Payment;
 import com.joaogodoi.hrpayroll.services.PaymentService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,9 +17,14 @@ public class PaymentResource {
     @Autowired
     private PaymentService paymentService;
 
+    @CircuitBreaker(name = "hr-worker-circuitbreaker", fallbackMethod = "getAlternative")
     @GetMapping(value = "/{workerId}/days/{days}")
     public ResponseEntity<Payment> get(@PathVariable Long workerId, @PathVariable Integer days) {
         Payment payment = paymentService.getPayment(workerId, days);
         return ResponseEntity.ok(payment);
+    }
+
+    public ResponseEntity<String> getAlternative(Long workerId, Integer days, Throwable e) {
+        return ResponseEntity.internalServerError().body("Workers API not available!");
     }
 }
