e2b158105ea42535d9b39687f63e95808da016a9
João Godoi <joaogodoi.dev@gmail.com>
2022-11-16 21:06:55 -0300

    :sparkles: Feat: Created a rest template and comunication between HR-Worker and HR-Payroll services

--- hr-payroll/src/main/java/com/joaogodoi/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/joaogodoi/hrpayroll/services/PaymentService.java
@@ -1,11 +1,29 @@
 package com.joaogodoi.hrpayroll.services;
 
 import com.joaogodoi.hrpayroll.entities.Payment;
+import com.joaogodoi.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
+
+    @Value("${hr-worker.host}")
+    private String hrWorkerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Payment getPayment(long workerId, int days) {
-        return new Payment("João", 500.0, days);
+        Map<String, String> uriVariables = new HashMap<String, String>();
+        uriVariables.put("id", "" + workerId);
+
+        Worker worker = restTemplate.getForObject(hrWorkerHost + "/{id}", Worker.class, uriVariables);
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
