a40eaf48332292b9468bb14369b7b26e3767fdab
João Godoi <joaogodoi.dev@gmail.com>
2022-11-18 00:04:54 -0300

    :sparkles: Feat: Created and configurated user resource

---
+++ hr-user/src/main/java/com/joaogodoi/hruser/resources/UserResource.java
@@ -0,0 +1,34 @@
+package com.joaogodoi.hruser.resources;
+
+import com.joaogodoi.hruser.entities.User;
+import com.joaogodoi.hruser.repositories.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RefreshScope
+@RestController
+@RequestMapping(value = "/user")
+public class UserResource {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserResource.class);
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        User user = userRepository.findById(id).get();
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping(value = "/search")
+    public ResponseEntity<User> getUser(@RequestParam String email) {
+        User user = userRepository.findByEmail(email);
+        return ResponseEntity.ok(user);
+    }
+
+}
